0846878eddb7a7ff65a29acba4e222002294c56c
northdigital <10321991+northdigital@users.noreply.github.com>
2020-09-19 21:07:54 +0300

    commit

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import schema
 from datetime import datetime
@@ -18,9 +18,22 @@ class Todo(db.Model):
     def __repr__(self):
         return "<Task %r>" % self.id
 
-@app.route("/")
+@app.route("/", methods=["GET", "POST"])
 def index():
-    return render_template("index.html")
+    if request.method == "POST":
+        task_content = request.form['content']
+        new_task = Todo(content=task_content)
+
+        try:
+            db.session.add(new_task)
+            db.session.commit()
+            return redirect("/")
+        except:
+            return "There was an issue with the task!"
+
+    else:
+        tasks = Todo.query.order_by(Todo.date_created).all()
+        return render_template("index.html", tasks=tasks)
 
 if __name__ == "__main__":
     app.run(debug=True)
